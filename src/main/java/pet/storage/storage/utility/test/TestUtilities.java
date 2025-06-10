1557a7b01e0f7efd5f0a81a004ff36adbf075389
Alexey Tarasov <del-fisha@yandex.ru>
2025-06-10 17:18:08 +0300

    Delete dublicate 2

--- src/main/java/pet/storage/storage/utility/test/TestUtilities.java
+++ src/main/java/pet/storage/storage/utility/test/TestUtilities.java
@@ -1,22 +1,26 @@
 package pet.storage.storage.utility.test;
 
 import org.springframework.stereotype.Component;
+import pet.storage.storage.dto.abstract_classes.ItemDTO;
 import pet.storage.storage.model.abstract_classes.Item;
 
 @Component
 public class TestUtilities {
 
-    public TestUtilities() {
-        System.out.println("***************************************************************************************");
-    }
      public boolean baseFieldsComparison(Item item, Item savedItem) {
 
-         System.out.println("Item id - " + item.getId());
-         System.out.println("Saved Item id - " + savedItem.getId());
-         System.out.println("Item name - " + item.getName());
-         System.out.println("Saved Item name - " + savedItem.getName());
-         System.out.println("Item date - " + item.getDateOfPurchase());
-         System.out.println("Saved Item date - " + savedItem.getDateOfPurchase());
+         return (item.getId() == savedItem.getId()
+                && item.getName().equals(savedItem.getName())
+                && item.getFabricator().equals(savedItem.getFabricator())
+                && item.getCategory() == savedItem.getCategory()
+                && item.getMetric() == savedItem.getMetric()
+                && item.getAmount() == savedItem.getAmount()
+                && item.getPrice() == savedItem.getPrice()
+                && item.getDateOfPurchase() == savedItem.getDateOfPurchase()
+                && item.getDescription().equals(savedItem.getDescription()));
+    }
+
+    public boolean baseFieldsComparison(Item item, ItemDTO savedItem) {
 
         return (item.getId() == savedItem.getId()
                 && item.getName().equals(savedItem.getName())
@@ -27,6 +31,5 @@ public class TestUtilities {
                 && item.getPrice() == savedItem.getPrice()
                 && item.getDateOfPurchase() == savedItem.getDateOfPurchase()
                 && item.getDescription().equals(savedItem.getDescription()));
-
     }
 }
